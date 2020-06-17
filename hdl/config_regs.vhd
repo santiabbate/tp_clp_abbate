dbd3988c9b41257b67f615e357984aacd9c794f6
Santiago Abbate <santiabbate@gmail.com>
2020-06-17 15:39:07 -0300

    Agregados menores

--- hdl/config_regs.vhd
+++ hdl/config_regs.vhd
@@ -18,18 +18,12 @@ end;
 architecture arch of config_regs is
 
 begin
-    -- config_reg_0 <= (others => '0');
-    -- config_reg_1 <= (others => '0');
-    -- config_reg_2 <= (others => '0');
-    -- config_reg_3 <= (others => '0');
-    -- config_reg_4 <= (others => '0');
-    -- config_reg_5 <= (others => '0');
-
     process(btn_i)
     begin
         case( btn_i ) is
 
-            when "0001" =>
+            when "0001" => 
+                -- Modo continuo sin modulación, frecuencia: 1 MHz.
                 config_reg_0 <= X"00000001";
                 config_reg_1 <= X"00000001";
                 config_reg_2 <= (others => '0');
@@ -38,6 +32,9 @@ begin
                 config_reg_5 <= (others => '0');
             
             when "0010" =>
+                -- Modo continuo modulado en frecuencia.
+                -- Frec: 1 MHz a 10 MHz.
+                -- Duración: 10 us.
                 config_reg_0 <= X"00000001";
                 config_reg_1 <= X"00000007";
                 config_reg_2 <= (others => '0');
@@ -46,6 +43,10 @@ begin
                 config_reg_5 <= X"0000F197";
             
             when "0100" =>
+                -- Modo continuo modulado en fase
+                -- Código Barker: 13
+                -- Ancho de subpulso: 2 us.
+                -- Frec: 1 MHz.
                 config_reg_0 <= X"00000001";
                 config_reg_1 <= X"00000003";
                 config_reg_2 <= (others => '0');
@@ -54,12 +55,16 @@ begin
                 config_reg_5 <= X"D0001F35";
             
             when "1000" =>
+                -- Modo pulsado sin modulación
+                -- Frec: 1MHz.
+                -- Período: 16 us.
+                -- Ancho de pulso: 5 us.
                 config_reg_0 <= X"00000001";
-                config_reg_1 <= X"00000003";
-                config_reg_2 <= (others => '0');
+                config_reg_1 <= X"00000006";
+                config_reg_2 <= X"027107D0";
                 config_reg_3 <= X"0083126E";
-                config_reg_4 <= X"051EB851";
-                config_reg_5 <= X"0000F197";
+                config_reg_4 <= (others => '0');
+                config_reg_5 <= (others => '0');
             when others =>
                 config_reg_0 <= (others => '0');
                 config_reg_1 <= (others => '0');
--- xdc/trabajo_final_ArtyZ7.xdc
+++ xdc/trabajo_final_ArtyZ7.xdc
@@ -1,23 +1,13 @@
-# ArtyZ7 xdc
-# LED [3:0]
-############################
-# On-board led             #
-############################
-#set_property -dict {PACKAGE_PIN R14 IOSTANDARD LVCMOS33} [get_ports {led_pins[0]}]
-#set_property -dict {PACKAGE_PIN P14 IOSTANDARD LVCMOS33} [get_ports {led_pins[1]}]
-#set_property -dict {PACKAGE_PIN N16 IOSTANDARD LVCMOS33} [get_ports {led_pins[2]}]
-#set_property -dict {PACKAGE_PIN M14 IOSTANDARD LVCMOS33} [get_ports {led_pins[3]}]
-#set_property -dict {PACKAGE_PIN M14 IOSTANDARD LVCMOS33} [get_ports {led_o}]
 
-# CLK source 50 MHz
+
+# CLK source 125 MHz
 set_property -dict {PACKAGE_PIN H16 IOSTANDARD LVCMOS33} [get_ports clk_i]
 create_clock -period 8.000 -name clk_pin -waveform {0.000 4.000} [get_ports clk_i]
 
 # Rst Switch[0]
 set_property -dict { PACKAGE_PIN M20  IOSTANDARD LVCMOS33 } [get_ports { resetn_i }]; #IO_L7N_T1_AD2N_35 Sch=SW0
 
-#set_property -dict { PACKAGE_PIN M19  IOSTANDARD LVCMOS33 } [get_ports { sw[1] }]; #IO_L7P_T1_AD2P_35 Sch=SW1
-
+# Botones
 set_property -dict { PACKAGE_PIN D19    IOSTANDARD LVCMOS33 } [get_ports { btn_i[0] }]; #IO_L4P_T0_35 Sch=BTN0
 set_property -dict { PACKAGE_PIN D20    IOSTANDARD LVCMOS33 } [get_ports { btn_i[1] }]; #IO_L4N_T0_35 Sch=BTN1
 set_property -dict { PACKAGE_PIN L20    IOSTANDARD LVCMOS33 } [get_ports { btn_i[2] }]; #IO_L9N_T1_DQS_AD3N_35 Sch=BTN2
